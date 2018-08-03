3d761354b874fec43451a966aed80eb7eb041a94
Cormac Neeson <neesonch@tcd.ie>
2018-08-03 20:54:27 +0100

    feat: display team defensive ranking on each team list item

--- src/components/TeamListItem.js
+++ src/components/TeamListItem.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react';
 export default class TeamListItem extends Component {
 
 	constructor(props) {
-		super(props);	
+		super(props);
+		this.state = {teamDefensiveRank: -1};
 	}
 
 	componentDidUpdate(prevProps){
 		if(prevProps.defenseRankings != this.props.defenseRankings){
-			const teamDefensiveRank = this.props.defenseRankings.findIndex(rank => rank.team === this.props.team.name);
+			const teamDefensiveRank = this.props.defenseRankings.findIndex(rank => {
+				return(rank.team === this.props.team.name && rank.index > -1)
+			});
 			this.setState({teamDefensiveRank});
 		}
 	}
 
 	render() {
 		return (
-			<div style = {styles.title}>{this.props.team.name}:
+			<div style = {styles.title}>
+				{this.props.team.name} (Defensive rank: {this.state.teamDefensiveRank > 0 ? this.state.teamDefensiveRank + 1 : 'n/a' });
 				<div style = {styles.container}>
 					{this.props.team.schedule.map((opponent, index) => <div style = {styles.item} key={index}>{opponent}</div>)}
 				</div>
